84e6ab94725c5644f4a956cb0241d239f75f7212
philippequentin91 <philippequentin91@gmail.com>
2016-05-30 14:36:46 -0400

    cration du formulaire d'inscription

---
+++ inscription.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>Inscription</title>
+    </head>
+    <body>
+        <form action="" method="POST">
+            <fieldset>
+                <legend>Inscription</legend>
+
+                <label for="">AdresseMail </label>
+                    <input type="email" name="email" required/><br><br>
+
+                <label for="">Pseudo </label>
+                    <input type="text" name="pseudo" required/><br><br>
+
+                <label for="">Mot de passe </label>
+                    <input type="password" name="mdp" required/><br><br>
+
+                <label for="">Confirmer Mot de passe </label>
+                    <input type="password" name="mdpConfirm" required/><br><br>
+
+                <button type="submit">S'inscrire</button>
+            </fieldset>
+        </form>
+    
+    </body>
+</html>
