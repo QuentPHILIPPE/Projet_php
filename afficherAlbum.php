9a4f379e94c5b1d62310500fe5bfb314afd7d397
projetphpsen <Simonvasseur91550@live.com>
2016-06-05 11:12:39 +0000

    affiche les albums de la db dans un tableau html

---
+++ afficherAlbum.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Films</title>
+</head>
+
+<body>
+  <table>
+    <thead>
+      <tr>
+        <th>Album</th>
+        <th>Date</th>
+        <th>Note</th>
+        <th>Lien</th>
+      </tr>
+    </thead>
+  <?php
+         require_once 'connexion.php';
+ 
+        $resultats=$connexion->query("SELECT A.nomAlbum,A.dateSortie,A.note, A.lienYoutube FROM Album A ");
+
+        while( $album = $resultats->fetch(PDO::FETCH_ASSOC) ) {
+ 
+
+                echo "<tr>";
+                echo "<td>" . $album['nomAlbum'] . "</td>" . "<td>" . $album['dateSortie'] . "</td>" . "<td>" . $album['note'] . "</td>" . "<td>" . $album['lienYoutube'] . "</td>";
+                echo "</tr>";
+        }
+        
+        $resultats->closeCursor();
+?>
+  </table>
+</body>
+</html>
