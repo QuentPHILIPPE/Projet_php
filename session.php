df4510d22cf789ceb9eabc66ff03e8c62f5c1485
QuentPHILIPPE <philippequentin91@gmail.com>
2016-06-06 22:19:27 +0200

    création variable session connexion utilisateur

---
+++ session.php
@@ -0,0 +1,48 @@
+<?php
+  session_start();
+  if(isset($_POST) && !empty($_POST['pseudo']) && !empty($_POST['mdp'])){
+    require_once 'connexion.php';
+    
+    $password = sha1($_POST['mdp']);
+    
+    $requete = $connexion->prepare("select * From utilisateur where pseudo = ? AND mdpU = ?");
+    $requete->execute([$_POST['pseudo'], $password]);
+    $userexist = $requete->rowCount();
+    
+    if($userexist == 1){
+      echo "vous etes co" . $_POST['pseudo'];  
+    } else {
+      echo "mauvais identifiant ou mauvais mot de passe";
+    }
+  }
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Connexion</title>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" href="style.css">
+
+</head>
+  
+<body>
+    
+    <form action="" method="POST">
+      <fieldset>
+        <legend>Connexion</legend>
+
+          
+        <label for="">Pseudo</label>
+        <input type="text" name="pseudo" placeholder="Pseudo" value="<?php if(isset($_POST['pseudo'])){ echo $_POST['pseudo']; } ?>" required /><br><br>
+
+        <label for="">Mot de passe </label>
+        <input type="password" name="mdp" placeholder="password"/><br><br>
+        
+        <button type="submit" id="submit">Connexion</button>
+      </fieldset>
+    </form>
+
+</body>
+
+</html>
