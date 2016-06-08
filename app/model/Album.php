5a8ca44e6ad05b128b9a7db4b8fc2585b9496bbc
QuentPHILIPPE <philippequentin91@gmail.com>
2016-06-08 14:56:04 +0200

    ajout de fonction telles que ajoutalbum

--- app/model/Album.php
+++ app/model/Album.php
@@ -1,64 +1,104 @@
 <?php 
 
 class Album {
-    public $idAlbum;
+    public $idAlbum; // int
     public $nomAlbum; //string
-    public $dateSortie;//string
-    public $note;
-		public $dateSortie;
-		public $dateSortie;
+		public $dateSortie;// date
+		public $note; // int
+		public $artiste;
+		public $lienLastFm;
     
-	function __construct($nom,$lien)    {
-        $this->nomArtiste = $nom;
-        $this->lienWiki = $lien;
-        
+	function __construct($nom,$note)	{
+        $this->id = $id;
+				$this->nomAlbum = $nom;
+				$this->dateSortie = $date;
+        $this->note = $note;
+				$this->artiste = $artiste;
+        $this->lienLastFm = $lien;
+		
         $db = Database::getInstance();
-		$sql = "INSERT INTO Artiste Art SET Art.nomArtiste = :nom, Art.lienWiki = :wiki";
+		$sql = "INSERT INTO Album SET nomAlbum = :nom, note = :note";
 		$stmt = $db->prepare($sql);
 		$stmt->setFetchMode(PDO::FETCH_ASSOC);
-		$stmt->execute($nom,$wiki);
+		$stmt->execute($nom,$note);
         
-        $sql = "SELECT Art.idArtiste FROM Artiste Art WHERE Art.nomArtiste = :nom";
+        $sql = "SELECT idAlbum FROM Album WHERE nomAlbum = :nom";
 		$stmt = $db->prepare($sql);
 		$stmt->setFetchMode(PDO::FETCH_ASSOC);
 		$stmt->execute($nom);
-		$this->idArtiste = $stmt->fetch(); 
+		$this->idAlbum = $stmt->fetch(); 
     }
     
     function getNom() {
         $db = Database::getInstance();
-		$sql = "SELECT Art.nomArtiste FROM Artiste Art WHERE Art.idArtiste = :id";
+		$sql = "SELECT nomAlbum FROM Album WHERE idAlbum = :id";
 		$stmt = $db->prepare($sql);
 		$stmt->setFetchMode(PDO::FETCH_ASSOC);
 		$stmt->execute($id);
 		return $stmt->fetch();
     }
     
-    function getWiki() {
+    function getDate() {
         $db = Database::getInstance();
-		$sql = "SELECT Art.lienWiki FROM Artiste Art WHERE Art.idArtiste = :id";
+		$sql = "SELECT dateSortie FROM Album WHERE idAlbum = :id";
+		$stmt = $db->prepare($sql);
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute($id);
+		return $stmt->fetch();
+    }
+  
+	
+		function getNote() {
+        $db = Database::getInstance();
+		$sql = "SELECT note FROM Album WHERE idAlbum = :id";
 		$stmt = $db->prepare($sql);
 		$stmt->setFetchMode(PDO::FETCH_ASSOC);
 		$stmt->execute($id);
 		return $stmt->fetch();
     }
     
+	
     function setNom($nom, $id) {
         $db = Database::getInstance();
-		$sql = "UPDATE Artiste Art SET Art.nomArtiste = :nom WHERE Art.idArtiste = :id";
+		$sql = "UPDATE Album SET nomAlbum = :nom WHERE idAlbum = :id";
 		$stmt = $db->prepare($sql);
 		$stmt->setFetchMode(PDO::FETCH_ASSOC);
 		$stmt->execute($nom,$id);
     }
     
-    function setWiki($wiki, $id) {
+    function setDate($date, $id) {
         $db = Database::getInstance();
-		$sql = "UPDATE Artiste Art SET Art.lienWiki = :wiki WHERE Art.idArtiste = :id";
+		$sql = "UPDATE Album SET dateSortie = :date WHERE idAlbum = :id";
 		$stmt = $db->prepare($sql);
 		$stmt->setFetchMode(PDO::FETCH_ASSOC);
-		$stmt->execute($wiki,$id);
+		$stmt->execute($date,$id);
     }
     
+		function setNote($note, $id) {
+        $db = Database::getInstance();
+		$sql = "UPDATE Album SET note = :note WHERE idAlbum = :id";
+		$stmt = $db->prepare($sql);
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute($note,$id);
+    }
+    
+	public static function getList() {
+	$db = Database::getInstance();
+	$sql = "SELECT * FROM album";
+	$stmt = $db->query($sql);
+	$stmt->setFetchMode(PDO::FETCH_CLASS, "Album");
+	return $stmt->fetchAll();
+		
+	}
+
+	public static function ajoutAlbum($nom,$date,$note,$artiste,$lien) {
+		$db = Database::getInstance();
+		$sql = "INSERT INTO album (nomAlbum,dateSortie,note,artiste,lien) VALUES (:nomAlbum,:dateSortie,:note,:artiste,:lien)";
+		$stmt = $db->prepare($sql);
+		//$stmt->setFetchMode(PDO::FETCH_CLASS, "Artiste");
+		echo "Album bien ajouté !";
+		return $stmt->execute([$nom,$date,$note,$artiste,$lien]);
+ 	}
 }
 
 
