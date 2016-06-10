aa078544b834c8e6e162366485bb1ed51ebe516a
QuentPHILIPPE <philippequentin91@gmail.com>
2016-06-10 15:12:07 +0200

    add artiste et utilisateur

--- app/kernel/Router.php
+++ app/kernel/Router.php
@@ -11,19 +11,19 @@ class Router {
 		 $result["action"] = "index";
       } else {
 		  $parts = explode("/", $query);
-		  if($parts[0] == "contact")  {
-			  if (count($parts) == 1){
-				  $result["controller"] = "Contact";
-				  $result['action'] = "afficherListe";
+		  
+				if($parts[0] == "contact")  {
+					if (count($parts) == 1){
+						$result["controller"] = "Contact";
+						$result['action'] = "afficherListe";
 			  }
+					
 			  if ((count($parts) == 2) && ($parts[1] == "afficher")){
             $result["controller"] = "Index";
             $result["action"] = "afficherListe";
             //$result["params"]["slug"] = $parts[1];            
 			  }
-			  if ( (count($parts) == 3) 
-				  && ($parts[1] == "afficher")
-				  && ($parts[0] == "contact")){
+			  if ( (count($parts) == 3)  && ($parts[1] == "afficher") && ($parts[0] == "contact")){
 
 					  $result["controller"] = "Contact";
 					  $result["action"] = "afficherContact";
@@ -31,13 +31,77 @@ class Router {
 				  }
 			  //
 
-			  if ((count($parts) == 3) && ($parts[1] == "modifier")){
+				if ((count($parts) == 3) && ($parts[1] == "modifier")){
 
 				  $result["controller"] = "Contact";
 				  $result["action"] = "modifierContact";
 				  $result["params"]["id"]= $parts[2];
 				  $result["params"]["post"]= $_POST;
 			  }
+				
+					
+				if($parts[0] == "utilisateur")  {
+					if (count($parts) == 1){
+						$result["controller"] = "Utilisateur";
+						$result['action'] = "afficherListe";
+			  }
+					
+			  if ((count($parts) == 2) && ($parts[1] == "afficher")){
+            $result["controller"] = "Index";
+            $result["action"] = "afficherListe";
+            //$result["params"]["slug"] = $parts[1];            
+			  }
+			  if ( (count($parts) == 3)  && ($parts[1] == "afficher") && ($parts[0] == "utilisateur")){
+
+					  $result["controller"] = "Utilisateur";
+					  $result["action"] = "afficherUtilisateur";
+					  $result["params"]["id"] = $parts[2];            
+				  }
+			  //
+
+				if ((count($parts) == 3) && ($parts[1] == "modifier")){
+
+				  $result["controller"] = "Utilisateur";
+				  $result["action"] = "modifierUtilisateur";
+				  $result["params"]["adresseMail"]= $parts[2];
+				  $result["params"]["post"]= $_POST;
+			  }	
+					
+				}	
+					
+				if($parts[0] == "artiste")  {
+					if (count($parts) == 1){
+						$result["controller"] = "Artiste";
+						$result['action'] = "afficherListe";
+					}
+			  if ((count($parts) == 2) && ($parts[1] == "afficher")){
+            $result["controller"] = "Index";
+            $result["action"] = "afficherListe";
+            //$result["params"]["slug"] = $parts[1];            
+			  }
+			  if ( (count($parts) == 3) && ($parts[1] == "afficher") && ($parts[0] == "artiste")){
+					  $result["controller"] = "Artiste";
+					  $result["action"] = "afficherArtiste";
+					  $result["params"]["id"] = $parts[2];        
+				}
+				
+				if ((count($parts) == 3) && ($parts[1] == "modifier")){
+
+				  $result["controller"] = "Artiste";
+				  $result["action"] = "modifierArtiste";
+				  $result["params"]["id"]= $parts[2];
+				  $result["params"]["post"]= $_POST;
+			  }	
+					
+				if ((count($parts) == 2) && ($parts[1] == "ajouterArtiste")) {
+				 $result["controller"] = "Artiste";
+				 $result["action"] = "ajouterArtiste";
+				 $result["params"]["post"]= $_POST;
+				 $result["redirection"] = "afficherArtiste";
+				}	
+					
+				}
+			  
 			  //
 		  }
 
