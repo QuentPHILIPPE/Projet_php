f3da1972f39bd2c3f45a9817cd1b3f966792d9e6
philippequentin91 <philippequentin91@gmail.com>
2016-06-14 11:28:32 -0400

    verif mdp

--- app/controller/InscriptionController.php
+++ app/controller/InscriptionController.php
@@ -9,39 +9,34 @@ class InscriptionController extends Controller {
 		$db = Database::getInstance();
 		extract($_POST);
 		
-		if(isset($adresseMail) && ($pseudo) && ($mdpU)) {	
-			$sql = "INSERT INTO utilisateur (adresseMail,pseudo,mdpU) VALUES (:adresseMail,:pseudo,:mdpU)";
+		if(isset($adresseMail) && ($pseudo) && ($mdpU)) {
+			if($mdpU == $mdpConfirm){
+				$sql = "INSERT INTO utilisateur (adresseMail,pseudo,mdpU) VALUES (:adresseMail,:pseudo,:mdpU)";
+				$mdpU = sha1($mdpU);
+				$stmt = $db->prepare($sql);
+				//$stmt->setFetchMode(PDO::FETCH_CLASS, "Utilisateur");
+				$stmt->bindParam(":adresseMail", $adresseMail);
+				$stmt->bindParam(":pseudo", $pseudo);
+				$stmt->bindParam(":mdpU", $mdpU);
+				
+				echo "Utilisateur bien créé";
+				
+				return $stmt->execute(array(
+				":adresseMail" => $adresseMail,
+				":pseudo" => $pseudo,
+				":mdpU" => $mdpU));
 			
-			$stmt = $db->prepare($sql);
-		//$stmt->setFetchMode(PDO::FETCH_CLASS, "Utilisateur");
-			$stmt->bindParam(":adresseMail", $adresseMail);
-			$stmt->bindParam(":pseudo", $pseudo);
-			$stmt->bindParam(":mdpU", $mdpU);
-		return $stmt->execute(array(
-			":adresseMail" => $adresseMail,
-			":pseudo" => $pseudo,
-			":mdpU" => $mdpU));
+				
 			
-			echo "Utilisateur bien créé";
-			
+			} else {
+				echo "mots de passe différents";
+			}
+		
 		} else {
 			echo "formulaire mal renseigné";
 		}
 	}
-	/*
-  public function verifInscription() {
-    extract($_POST);
-    if(isset($pseudo) && isset($adresseMail) && isset ($mdp) && isset($mdpConfirm)) {
-      if($mdp != $mdpConfirm) {
-        echo "Les mots de passe ne correspondent pas";
-      }
-      else {
-        $this->view->inscription = Inscription::envoiInscription($adresseMail,$pseudo,$mdp);
-				$this->view->display();
-      } 
-  	}		
-	}
-*/
+	
 	
 }
 ?>
