b5b01f99904fbc4d8e2834f64d0a8f948ffe906d
philippequentin91 <philippequentin91@gmail.com>
2016-06-15 03:06:00 -0400

    changement bouton submit note

--- app/model/Commentaire.php
+++ app/model/Commentaire.php
@@ -1,14 +1,14 @@
 <?php
   class Commentaire extends Model{
-    public $idCommentaire, $album, $pseudo, $message;
+    public $idCommentaire, $pseudo, $album, $message;
     
    public static function ajouterCommentaire($pseudo,$album,$message) {
     $db = Database::getInstance();
-    $sql = "INSERT INTO commentaire (pseudo,album,message) VALUES (:pseudo,:album,:message)";
+    $sql = "INSERT INTO commentaire (pseudo,album,message) VALUES (:pesudo,:album,:message)";
     $stmt = $db->prepare($sql);
     $stmt->setFetchMode(PDO::FETCH_CLASS, "Commentaire");
     return $stmt->execute(array(
-      ':pseudo' => $pseudo,
+      ':pseudo' => $_SESSION['pseudo'],
       ':album' => $album,
       ':message' =>$message
     ));
